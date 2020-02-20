a6e13744e0d6727957a0dbdb91e390160ac3eda1
Keun Kyeong <woodeekim@gmail.com>
2020-02-21 01:11:08 +0900

    Add JDBCtests.class

---
+++ src/test/java/org/zerock/persistence/JDBCTests.java
@@ -0,0 +1,32 @@
+package org.zerock.persistence;
+
+import lombok.extern.log4j.Log4j2;
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import static org.junit.Assert.fail;
+@Log4j2
+public class JDBCTests {
+    static {
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testConnection(){
+        try(Connection con =
+                    DriverManager.getConnection(
+                            "jdbc:oracle:thin:@localhost:1521:XE",
+                            "spring_board",
+                            "1111")){
+            log.info(con);
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
+}
