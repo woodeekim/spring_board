ffe6702366297d38d2a223070001860c1017b85d
Keun Kyeong <woodeekim@gmail.com>
2020-02-21 02:26:50 +0900

    Add DataSourceTest.class using HikariCP

--- src/main/java/org/zerock/config/RootConfig.java
+++ src/main/java/org/zerock/config/RootConfig.java
@@ -1,9 +1,26 @@
 package org.zerock.config;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan(basePackages = {"org.zerock.sample"})
 public class RootConfig {
+
+    @Bean
+    public DataSource dataSource(){
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setDriverClassName("oracle.jdbc.driver.OracleDriver");
+        hikariConfig.setJdbcUrl("jdbc:oracle:thin:@localhost:1521:XE");
+        hikariConfig.setUsername("spring_board");
+        hikariConfig.setPassword("1111");
+
+        HikariDataSource dataSource = new HikariDataSource(hikariConfig);
+        return dataSource;
+    }
 }
---
+++ src/test/java/org/zerock/persistence/DataSourceTests.java
@@ -0,0 +1,34 @@
+package org.zerock.persistence;
+
+import static org.junit.Assert.fail;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j2;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.config.RootConfig;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {RootConfig.class})
+@Log4j2
+public class DataSourceTests {
+    @Setter(onMethod_ = {@Autowired})
+    private DataSource dataSource;
+
+    @Test
+    public void testConnection(){
+
+        try(Connection con = dataSource.getConnection()){
+            log.info(con);
+        }catch (Exception e){
+            fail(e.getMessage());
+        }
+    }
+
+}
