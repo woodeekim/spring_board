2f9832d9f55d7622c5b9600dcbf9a1a1b9da283a
Keun Kyeong <woodeekim@gmail.com>
2020-02-20 22:43:39 +0900

    init git

---
+++ src/main/java/org/zerock/sample/Chef.java
@@ -0,0 +1,11 @@
+package org.zerock.sample;
+
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
+public class Chef {
+
+
+}
---
+++ src/main/java/org/zerock/sample/SampleHotel.java
@@ -0,0 +1,17 @@
+package org.zerock.sample;
+
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+import org.springframework.stereotype.Component;
+
+@Component
+@ToString
+@Getter
+@RequiredArgsConstructor
+public class SampleHotel {
+
+    private final Chef chef;
+
+}
