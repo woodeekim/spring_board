c8cc542faa015c06c51c4fcd07b431c53066bac3
Keun Kyeong <woodeekim@gmail.com>
2020-03-10 01:43:55 +0900

    Update SampleControllerTests.java for REST API test

--- src/test/java/org/woodee/controller/SampleControllerTests.java
+++ src/test/java/org/woodee/controller/SampleControllerTests.java
@@ -1,23 +1,70 @@
 package org.woodee.controller;
 
 
+import com.google.gson.Gson;
 import lombok.Setter;
 import lombok.extern.log4j.Log4j2;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.woodee.domain.Ticket;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 import java.util.ArrayList;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration("file:src/main/webapp/WEB-INF/dispatcher-servlet.xml")
+@ContextConfiguration({
+        "file:src/main/webapp/WEB-INF/applicationContext.xml",
+        "file:src/main/webapp/WEB-INF/dispatcher-servlet.xml"
+})
+@WebAppConfiguration
 @Log4j2
 public class SampleControllerTests {
 
     @Setter(onMethod_ = {@Autowired})
+    private WebApplicationContext ctx;
+
+    @Setter(onMethod_ = {@Autowired})
     private SampleController sampleController;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(ctx).build();
+    }
+
+    @Test
+    public void testConvert() throws Exception {
+        //외부에서 /ticket/+Post 형식으로 파라미터 보내면 @RequestBody를 통해 잘 받는지 테스트
+        Ticket ticket = new Ticket();
+        ticket.setTno(123);
+        ticket.setOwner("Admin");
+        ticket.setGrade("AAA");
+
+        //유닛테스트에서 JSON 형태로 만드는 법 (Gson 라이브러리를 사용)
+        //외부에서 JSON 형태로 보내기 때문에 ticket 을 JSON 형태로 만든다.
+        String jsonStr = new Gson().toJson(ticket);
+
+        log.info(jsonStr);
+
+        mockMvc.perform(post("/sample/ticket")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonStr))
+                .andExpect(status().is(200));
+
+
+    }
 
     @Test
     public void testController(){
